e7332c1eda6a26d274c78a5a96b945c165c0337a
Ville Koskela <vkoskela@gmail.com>
2021-09-05 20:17:10 +0300

    2.6 done

--- osa2/README.md
+++ osa2/README.md
@@ -5,7 +5,8 @@
 - [2.3: kurssitiedot, step8](kurssitiedot) *Already included in 2.2*
 - [2.4: kurssitiedot, step9](kurssitiedot)
 - [2.5: erillinen moduuli](kurssitiedot) *Already included in osa1*
+- [2.6: puhelinluettelo step1](puhelinluettelo)
 
-Tehty: 5/20
+Tehty: 6/20
 
 **[palaa juureen](../README.md)**
---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const nameAdder = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const nameChanger = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={nameAdder}>
+        <div>
+          name: <input value={newName} onChange={nameChanger} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => <Person key={person.name} person={person} />)
+      }
+    </div>
+  )
+
+}
+
+export default App
