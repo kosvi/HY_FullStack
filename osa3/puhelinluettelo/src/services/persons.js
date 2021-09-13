b33b657ff410f703e9910411c0f017cf0e97a687
Ville Koskela <vkoskela@gmail.com>
2021-09-13 12:47:08 +0300

    3.11 done

--- osa3/puhelinluettelo/src/services/persons.js
+++ osa3/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = async () => {
     // according to https://github.com/axios/axios#example
