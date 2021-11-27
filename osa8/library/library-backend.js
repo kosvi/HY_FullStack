30ba72bdd5366f476c6d2861f8398f96913087eb
Ville Koskela <vkoskela@gmail.com>
2021-11-27 20:08:45 +0200

    8.15 done!

--- osa8/library/library-backend.js
+++ osa8/library/library-backend.js
@@ -1,10 +1,8 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
 const config = require('./utils/config')
-// v1??? source: https://fullstackopen.com/osa8/graph_ql_palvelin
-// const { v1: uuid } = require('uuid')
 
 console.log('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI).then(() => {
@@ -13,37 +11,6 @@ mongoose.connect(config.MONGODB_URI).then(() => {
   console.error('error connecting to MongoDB:', error.message)
 })
 
-/*
-const typeDefs = gql`
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks: [Book]!
-  }
-  type Author {
-    name: String!
-    born: Int
-    bookCount: Int!
-    id: ID!
-  }
-  type Book {
-    title: String!
-    published: Int!
-    author: Author!
-    genres: [String!]!
-    id: ID!
-  }
-`
-const resolvers = {
-  Query: {
-    bookCount: () => Book.collection.countDocuments(),
-    authorCount: () => Author.collection.countDocuments(),
-    allBooks: async (root, args) => {
-      return await Book.find({})
-    }
-  }
-}
-*/
 const typeDefs = gql`
   type Query {
     bookCount: Int!
@@ -111,14 +78,26 @@ const resolvers = {
           })
         }
       }
-      const book = new Book({ title: args.title, published: args.published, author: author, genres: args.genres })
-      return await book.save()
+      try {
+        const book = new Book({ title: args.title, published: args.published, author: author, genres: args.genres })
+        return await book.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args
+        })
+      }
     },
     editAuthor: async (root, args) => {
       const author = await Author.findOne({ name: args.name })
       if (author) {
         author.born = args.setBornTo
-        return await author.save()
+        try {
+          return await author.save()
+        } catch (error) {
+          throw new UserInputError(error.message, {
+            invalidArgs: args
+          })
+        }
       }
       return null
     }
@@ -133,9 +112,9 @@ const resolvers = {
         return prev
       }
       */
-     // does not work for now!
-      const author = Author.findOne({ name: root.name })
-      return await (await Book.find({ author: author.id })).length // .reduce(reducer, 0)
+      const author = await Author.findOne({ name: root.name })
+      return await (await Book.find({ author: author })).length
+      //      return await (await Book.find({}).populate('author')).filter(b => b.author.id === author.id).length // .reduce(reducer, 0)
     }
   }
 }
