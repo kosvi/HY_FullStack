745672aef96c9493b907c8a32dd1571a97c1a9ec
Ville Koskela <vkoskela@gmail.com>
2021-12-04 21:01:10 +0200

    9.10 done!

--- osa9/patientor_backend/src/index.ts
+++ osa9/patientor_backend/src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express';
+import diagnoseRouter from './routers/diagnoses';
 import cors from 'cors';
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
-    console.log('someone pinged here');
-    res.send('pong');
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
 });
 
+app.use('/api/diagnoses', diagnoseRouter);
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
